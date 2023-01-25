e94bf4a5833fdc44b9633ce1710a16d7a1831b18
Michal <michalkalina23@gmail.com>
2023-01-25 17:07:51 +0100

    Methods implemented and tested

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,47 @@
+package com.serenitydojo;
+
+public class Dog {
+
+    private String name;
+    private String favoriteToy;
+    private int age;
+
+    public Dog(String name, String favoriteToy, int age) {
+        this.name = name;
+        this.favoriteToy = favoriteToy;
+        this.age =age;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String makeNoise() {
+        return "Woof";
+    }
+
+    public boolean isFed() {
+        return true;
+    }
+
+    public boolean feed() {
+        return true;
+    }
+}
